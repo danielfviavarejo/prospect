95e9427f0548877498d535b9b1642e9481b691ff
daniel <deecomunicacao@gmail.com>
2024-03-23 12:05:05 -0300

    start

---
+++ src/main/java/com/daniel/prospect001/Prospect001Application.java
@@ -0,0 +1,13 @@
+package com.daniel.prospect001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prospect001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prospect001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/prospect001/Prospect001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.prospect001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prospect001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
